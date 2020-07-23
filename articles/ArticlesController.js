7a479d9de822a3b41ec15435b58884c2ab44190b
Everton Borges <evertonbrc@hotmail.com>
2020-07-23 18:58:06 -0300

    Sistema de paginação

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -93,4 +93,34 @@ router.post("/article/update", (req, res) => {
     })
 })
 
+router.get("/articles/page/:num", (req, res) =>{
+    var page = req.params.num;
+    var offset = 0;
+
+    if(isNaN(page) || page == 1){
+        offset = 0;
+    }else{
+        offset = (parseInt(page) - 1) * 4;
+    }
+
+    Article.findAndCountAll({
+        limit: 4,
+        offset: offset
+    }).then(articles => {
+        var next;
+
+        if(offset + 4 >= articles.count){
+            next = false;
+        }else{
+            next = true;
+        }
+
+        var result = {
+            next: next,
+            articles : articles 
+        }
+        res.json(result);
+    })
+})
+
 module.exports = router;
